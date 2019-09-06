f7ad2813d54e483d1c8afeda011504c72a8939b6
luckky_zhou <luckky_zhou@163.com>
2019-09-05 21:36:10 -0700

    成功用python输入输出链表。关键在于定义链表结构，并用next输入值，用val输出值，输出时也需要重新定义一个新链表。定义head指针的意义在于可以输出链表。

--- 2_1.py
+++ 2_1.py
@@ -1,46 +1,43 @@
 # -*- coding:utf-8 -*-
 
 class ListNode(object):
-    def __init__(self):
-        self.val = None
+    def __init__(self,val):
+        self.val = val
         self.next = None
 
-class ListNode_handle():
-    def __init__(self):
-        self.cur_node = None
-
-    def printListNode(self,node):
-        while node:
-            print('\nnode:', node, 'value:', node.val, 'next:', node.next)
-            node = node.next
-
-def addTwoNumbers(self, l1:ListNode,l2:ListNode):
-    re = ListNode(0)
-    r = re
+def addTwoNumbers(l1:ListNode,l2:ListNode):
+    node = ListNode(0)
+    cur = node
     carry = 0
     while (l1 or l2):
         x = l1.val if l1 else 0
         y = l2.val if l2 else 0
         s = carry + x + y
-        carry=s//10
-        r.next=ListNode(s%10)
-        r=r.next
-        if(l1!=None):l1=l1.next
-        if(l2!=None):l2=l2.next
+        carry = s // 10
+        node.next = ListNode(s % 10)
+        node = node.next
+        if(l1 != None):l1 = l1.next
+        if(l2 != None):l2 = l2.next
     if(carry>0):
-        r.next=ListNode(1)
-    return re.next
-
-def inputListNode(self):
-    listnode1 = ListNode(0)
-    listnode1.next = ListNode(2)
-    #listnode1.next = listnode1
-    return ListNode_handle.printListNode(listnode1)
+        node.next = ListNode(1)
+    return cur.next
 
 if __name__ == '__main__':
-    inputListNode(1)
-    #listnode1.next = ListNode(2)
-    #listnode2 = ListNode(2)
-    #listnode2.next = ListNode(3)
-    #output = addTwoNumbers(listnode1,listnode2)
-    #print(output)
+    node1 = ListNode(2)
+    head1 = node1
+    node1.next = ListNode(4)
+    node1 = node1.next
+    node1.next = ListNode(3)
+
+    node2 = ListNode(5)
+    head2 = node2
+    node2.next = ListNode(6)
+    node2 = node2.next
+    node2.next = ListNode(4)
+
+    node = addTwoNumbers(head1,head2)
+
+    while node != None:
+        print(node.val)
+        node = node.next
+
