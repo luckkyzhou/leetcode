c7e67826cd9ed3d3f100d8488720b755ef6e17d3
luckky_zhou <luckky_zhou@163.com>
2019-09-06 14:40:43 -0700

    146学习

---
+++ 146.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+class ListNode(object):
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
