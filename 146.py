80954181da3b759f110474a4a1946ea51463d399
luckky_zhou <luckky_zhou@163.com>
2019-09-06 14:53:02 -0700

    it is really difficult

--- 146.py
+++ 146.py
@@ -6,3 +6,18 @@ class ListNode(object):
         self.value = value
         self.next = None
         self.prev = None
+
+class LRUCache:
+    def __init__(self,capacity:int):
+        self.capacity = capacity
+        self.hashmap = {}
+        # 新建头指针和尾指针
+        self.head = ListNode()
+        self.tail = ListNode()
+        # 初始化链表为head<->tail
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    # 定义将链表中某个节点移到末尾
+    def moveNodetoTail(self,key):
+        node = self.hashmap[key]
