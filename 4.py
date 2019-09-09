4ab418003f58bdbea290f91b1bde130fea3bcc82
luckky_zhou <luckky_zhou@163.com>
2019-09-09 11:15:20 -0700

    第4题一刷，最简单的方法

---
+++ 4.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class Solution:
+    def findMedianSortedArrays(self, nums1:List[int], nums2:List[int]) -> float:
+        m = len(nums1)
+        n = len(nums2)
+        nums1.extend(nums2)
+        nums1.sort()
+
+        if (m + n) & 1:
+            return nums1[(m + n - 1) >> 1]
+        else:
+            return (nums1[(m + n -1) >> 1] + nums1[(m + n) >> 1]) / 2
+
+if __name__ == '__main__':
+    find = Solution()
+    output = find.findMedianSortedArrays([1,2], [3,4])
+    print(output)
