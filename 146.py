abcab4909066e58a73b4ed90601955ce10df5eff
luckky_zhou <luckky_zhou@163.com>
2019-09-08 22:35:08 -0700

    146一刷

--- 146.py
+++ 146.py
@@ -21,3 +21,22 @@ class LRUCache:
     # 定义将链表中某个节点移到末尾
     def moveNodetoTail(self,key):
         node = self.hashmap[key]
+        # 将node置于prev和next中间
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        # 将node插入到尾指针前
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev.next = node
+        self.tail.prev = node
+
+    # 获取数据
+    def get(self,key:int):
+        # 如果已经在链表中，把它移到队尾变为最新访问的
+        if key in self.hashmap:
+            self.moveNodetoTail(key)
+        # 找不到key时返回-1
+        res = self.hashmap.get(key,-1)
+        if res == -1:
+            return res
+        
