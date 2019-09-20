5cf14338d15a5d9f479f73ba329b8f383c91d2f6
luckky_zhou <luckky_zhou@163.com>
2019-09-19 19:59:07 -0700

    23题二刷

--- 23_2.py
+++ 23_2.py
@@ -10,25 +10,22 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        l1 = ListNode(0)
-        cur = l1
+        res = ListNode(0)
+        cur = res
         head = []
 
-        # 将所有链表的头结点放入堆中
         for i in range(len(lists)):
             if lists[i]:
-                # 将指定的值放入堆head中
-                heapq.heappush(head, (lists[i].val, i))
+                heapq.heappush(head, (lists[i], i))
                 lists[i] = lists[i].next
 
-        # 推出堆中最小的元素，链表指针向后移，将链表指针的值加入堆中进行循环
         while head:
-            # 将堆中最小值给pop出来
             val, idx = heapq.heappop(head)
             cur.next = ListNode(val)
             cur = cur.next
-            if lists[idx]:
-                heapq.heappush(head, (lists[idx].val, idx))
-                lists[idx] = lists[idx].next
-        return l1.next
 
+            if lists[idx]:
+                heapq.heappush(head, (lists[idx], idx))
+                lists[idx] = lists[idx].next
+
+        return res.next
