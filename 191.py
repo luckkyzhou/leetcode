d91ce89f7d358c0f9fde3c4f749bec4fe72beab2
luckky_zhou <luckky_zhou@163.com>
2019-09-22 17:03:26 -0700

    小改动

--- 191.py
+++ 191.py
@@ -4,10 +4,10 @@ from typing import List
 
 class Solution:
     def hammingWeight(self, n):
-        ans = 0
+        res = 0
 
         while n:
             m = n & 1
-            ans += m
+            res += m
             n = n >> 1
-        return ans
+        return res
