3166be0aeb5a85b2e9ce19dd8093011b8fa3dc13
luckky_zhou <luckky_zhou@163.com>
2019-09-22 20:55:25 -0700

    105题一刷，树的结构，递归。

---
+++ 105.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Solution:
+
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if len(preorder) == 0 or len(inorder) == 0:
+            return None
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+
+        return root
