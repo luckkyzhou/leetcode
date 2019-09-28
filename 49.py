1025a6d84463a1a70c355a89d1d4cbe481e54b3d
luckky_zhou <luckky_zhou@163.com>
2019-09-27 23:16:23 -0700

    小改动

--- 49.py
+++ 49.py
@@ -2,7 +2,7 @@
 from typing import List
 
 class Solution:
-    def groupAnagrams(self, strs: List[str]):# -> List[List[str]]:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         strDict = {}
         res = []
         for _str in strs:
