4457531b9bc6d5de94c73d8722baaada7b9a2b74
luckky_zhou <luckky_zhou@163.com>
2019-09-27 22:04:26 -0700

    更简洁

--- 33_1.py
+++ 33_1.py
@@ -4,5 +4,5 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target not in nums: return -1
-        else: return nums.index(target)
+        if target in nums: return nums.index(target)
+        else: return -1
