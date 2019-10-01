af15763fc492b51e2e12146eb00e7adb7fcec8c1
luckky_zhou <luckky_zhou@163.com>
2019-09-30 21:07:02 -0700

    hun yi xia

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from collections import deque
+import sys
+from string import ascii_lowercase
 if __name__ == '__main__':
     '''
     num_list = [[0] * 2 for i in range(2)]
@@ -34,8 +36,34 @@ if __name__ == '__main__':
     m = [0 for x in range(4)]
     for i in range(1, 4):
         print(m[i])
-    '''
+    
     res = 'Twenty '
     while res[-1] == ' ':
         res = res[:-1]
     print(res)
+    
+    list = [1, 2, 3, 4]
+    print(list.index(3))
+    
+    test_set = set()
+    test_list = ['one','two','three','one']
+    for i in range(len(test_list)):
+        test_set.add(test_list[i])
+    print(test_set)
+
+    list = [1,2,3,4,5]
+    for i in range(0,3,2):
+        print(i)
+    
+
+    d1 = {}
+    key = 1
+    value = 2
+    d1.setdefault(key, []).append(value)
+    key = 1
+    value = 3
+    d1.setdefault(key, []).append(value)
+    print(d1)
+    '''
+    duplicates = [2 * ch for ch in ascii_lowercase]
+    print(duplicates)
