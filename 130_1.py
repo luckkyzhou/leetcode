f0d3b446fa195cfeb19874e6b2a44c558423f49e
luckky_zhou <luckky_zhou@163.com>
2019-10-14 17:10:21 -0700

    换一种更简洁的写法

---
+++ 130_1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+class Solution:
+    directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+    def solve(self, board: List[List[str]]) -> None:
+        m = len(board)
+        if not board or m == 0:
+            return
+        n = len(board[0])
+
+        for i in range(m):
+            self.DFS(board, i, 0)
+            self.DFS(board, i, n - 1)
+        for j in range(n):
+            self.DFS(board, 0, j)
+            self.DFS(board, m - 1, j)
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                if board[i][j] == '-':
+                    board[i][j] = 'O'
+        return
+
+    def DFS(self, board: List[List[str]], i: int, j: int):
+        m = len(board)
+        n = len(board[0])
+        if i < 0 or j < 0 or i >= m or j >= n or board[i][j] != 'O':
+            return
+        board[i][j] = '-'
+        for direction in self.directions:
+            self.DFS(board, i + direction[0], j + direction[j])
+        return
