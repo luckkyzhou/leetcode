e510cf1787df956f45eae8b93f856a858255154b
luckky_zhou <luckky_zhou@163.com>
2019-11-03 19:40:11 -0800

    lambda排序

--- 937.py
+++ 937.py
@@ -3,12 +3,12 @@ from typing import List
 
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        strList = []
-        numList = []
+        strings = []
+        nums = []
         for log in logs:
             if log[-1].isalpha():
-                strList.append(log)
+                strings.append(log)
             else:
-                numList.append(log)
-        strList.sort(key=lambda x:(x[x.index(' ') + 1]))
-        return strList + numList
+                nums.append(log)
+        strings.sort(key=lambda x:(x[x.index(' ') + 1:], x[:x.index(' ') + 1]))
+        return strings + nums
