4796644545a280d8dd82f356ee6f8320d3e31b6c
luckky_zhou <luckky_zhou@163.com>
2019-11-13 13:12:56 -0800

    DP，要考虑之前的状态

--- 276.py
+++ 276.py
@@ -2,7 +2,7 @@ class Solution:
     def numWays(self, n: int, k: int) -> int:
         if n == 0: return 0
         elif n == 1: return k
-        
+
         dp = [0 for _ in range(n + 1)]
         dp[1] = k
         dp[2] = k * k
