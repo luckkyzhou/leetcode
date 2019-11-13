764e9d1ac0e82696b3b50d84519449f1d6be79af
luckky_zhou <luckky_zhou@163.com>
2019-11-13 09:40:03 -0800

    dp 相邻和不相邻

---
+++ 276.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+        if n == 0: return 0
+        elif n == 1: return k
+        
+        dp = [0 for _ in range(n + 1)]
+        dp[1] = k
+        dp[2] = k * k
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 2] * (k - 1) + dp[i - 1] * (k - 1)
+        return dp[n]
