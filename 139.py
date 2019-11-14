eba9f98d5eee2446b3cd669d5a42f1cb6cd9e866
luckky_zhou <luckky_zhou@163.com>
2019-11-13 23:39:07 -0800

    DP on^2

---
+++ 139.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False for _ in range(len(s) + 1)]
+        dp[0] = True
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j : i] in wordDict:
+                    dp[i] = True
+                    break
+        return dp[-1]
+
+if __name__ == '__main__':
+    solution = Solution()
+    res = solution.wordBreak("leetcode",["leet","code"])
+    print(res)
