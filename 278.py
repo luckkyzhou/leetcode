f9cdcea5eaddbaa1884e0ae94f9b0248d53e022e
luckky_zhou <luckky_zhou@163.com>
2019-11-19 00:58:22 -0800

    二分搜索

---
+++ 278.py
@@ -0,0 +1,13 @@
+def isBadVersion (version):
+    pass
+class Solution:
+    def firstBadVersion(self, n):
+        left = 1
+        right = n
+        while left < right:
+            mid = left + (right - left) // 2
+            if not isBadVersion(mid):
+                left = mid + 1
+            else:
+                right = mid
+        return left
