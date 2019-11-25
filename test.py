5767acf0fd7e49eb560869277fe5032a25c9e922
luckky_zhou <luckky_zhou@163.com>
2019-11-24 23:42:28 -0800

    打卡

--- test.py
+++ test.py
@@ -1,72 +1,12 @@
 from collections import deque
 import sys
 from string import ascii_lowercase
-if __name__ == '__main__':
-    '''
-    num_list = [[0] * 2 for i in range(2)]
-    print(num_list)
-    dp = [[False for _ in range(3)] for _ in range(3)]
-    print(dp)
-
-    s = 'abcgefcba'
-    l = 1
-    r = 3
-    res = s[l:r + 1]
-    print(res)
-    
-    grid = [['1', '1', '1', '1', '0'],
-            ['1', '1', '0', '1', '0'],
-            ['1', '1', '0', '0', '0'],
-            ['0', '0', '0', '0', '0']]
-    m = len(grid)
-    n = len(grid[0])
-    queue = deque()
-    #queue.append((1,1,1))
-    if queue:
-        x, y, z = queue.popleft()
-        print(x,y)
-        print(queue)
-    else:
-        print(m)
-    
-
-    n = [1, 2, 3, 4, 5]
-    #print(n[-1])
-
-    m = [0 for x in range(4)]
-    for i in range(1, 4):
-        print(m[i])
-    
-    res = 'Twenty '
-    while res[-1] == ' ':
-        res = res[:-1]
-    print(res)
-    
-    list = [1, 2, 3, 4]
-    print(list.index(3))
-    
-    test_set = set()
-    test_list = ['one','two','three','one']
-    for i in range(len(test_list)):
-        test_set.add(test_list[i])
-    print(test_set)
-
-    list = [1,2,3,4,5]
-    for i in range(0,3,2):
-        print(i)
-    
-
-    d1 = {}
-    key = 1
-    value = 2
-    d1.setdefault(key, []).append(value)
-    key = 1
-    value = 3
-    d1.setdefault(key, []).append(value)
-    print(d1)
-
-    duplicates = [2 * ch for ch in ascii_lowercase]
-    print(duplicates)
-    '''
+import collections
+from typing import List
+import heapq
 
 
+test = [(2, 0),(6, 1),(5, 1)]
+test.sort(key=lambda x: (x[1], x[0]))
+test1 = x[0] for x in test
+print(test1)
