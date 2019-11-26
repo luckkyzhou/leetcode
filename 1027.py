413d1fba93f2696111aaeecf3decc9da855bbabf
luckky_zhou <luckky_zhou@163.com>
2019-11-25 20:41:26 -0800

    DP

---
+++ 1027.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def longestArithSeqLength(self, A: List[int]) -> int:
+        res = 1
+        dp = [{} for i in range(len(A))]
+
+        for i in range(1, len(A)):
+            for j in range(i):
+                diff = A[i] - A[j]
+                n = dp[j].get(diff, 1) + 1
+                dp[i][diff] = n
+            res = max(res, max(arr[i].values()))
+        return res
