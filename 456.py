1b3b9b4fefb4b4eeeaf9c4029176910e921fe112
luckky_zhou <luckky_zhou@163.com>
2019-12-05 10:58:12 -0800

    stack

---
+++ 456.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        second = -2**31
+
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] < second: return True
+            while stack and nums[i] > stack[-1]:
+                second = stack.pop()
+            stack.append(nums[i])
+        return False
