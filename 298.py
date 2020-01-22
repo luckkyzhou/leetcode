1a663249743ad7864ab4cc74e0e613b13acf9cef
luckky_zhou <luckky_zhou@163.com>
2020-01-22 14:31:47 -0800

    DFS，中序遍历

---
+++ 298.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Solution:
+    def __init__(self):
+        self.res = 0
+    def longestConsecutive(self, root: TreeNode) -> int:
+        def DFS(root: TreeNode, parent: TreeNode, length):
+            if not root: return
+
+            if root.val == parent.val + 1: length += 1
+            else: length = 1
+            self.res = max(self.res, length)
+            DFS(root.left, root, length)
+            DFS(root.right, root, length)
+        DFS(root, None, 0)
+        return self.res
