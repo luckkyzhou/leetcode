7f148a9375b74483265ecb16e08f0c8b2831db90
luckky_zhou <luckky_zhou@163.com>
2020-02-02 21:35:00 -0800

    打卡

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 from collections import deque
+from collections import defaultdict
 import sys
 from string import ascii_lowercase
 import collections
 from typing import List
 import heapq
 import math
+import random
+import itertools
 
+if __name__ == '__main__':
+    hashmap = defaultdict(int)
+    hashmap[(1,2)] = 1
+    print(hashmap)
