562efc9b481036e580f9e13466baa95887a3092b
luckky_zhou <luckky_zhou@163.com>
2020-02-26 20:36:38 -0800

    非ascii字符

---
+++ 271.py
@@ -0,0 +1,20 @@
+import
+class Codec:
+
+    def encode (self, strs):
+        """Encodes a list of strings to a single string.
+
+        :type strs: List[str]
+        :rtype: str
+        """
+        if strs == []: return "null"
+        return chr(257).join(strs)
+
+    def decode (self, s):
+        """Decodes a single string to a list of strings.
+
+        :type s: str
+        :rtype: List[str]
+        """
+        if s == "null": return []
+        return s.split(chr(257))
