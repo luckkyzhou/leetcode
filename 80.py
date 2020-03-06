591e55a67ad653d8c3fed5b36ed40a08989b992e
luckky_zhou <luckky_zhou@163.com>
2020-03-05 20:14:24 -0800

    快慢指针

---
+++ 80.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        for j in range(len(nums)):
+            if i < 2 or nums[j] != nums[i - 2]:
+                nums[i] = nums[j]
+                i += 1
+        return i
