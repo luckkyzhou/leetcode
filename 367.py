d442790e28dd9f48860504438ef2c7e396d33faf
luckkyzhou <luckkyzhou@gmail.com>
2020-08-12 23:30:55 -0700

    binary search

---
+++ 367.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 1:
+            return True
+        
+        left = 1
+        right = num
+        while left < right:
+            mid = left + (right - left) // 2
+            if mid * mid < num:
+                left = mid + 1
+            elif mid * mid > num:
+                right = mid
+            elif mid * mid == num:
+                return True
+        return False
